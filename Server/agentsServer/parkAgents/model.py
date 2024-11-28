5d065d1a2c83bfadf72333a4440b92baa3504772
BassedWarrior <bassedwarrior724@outlook.com>
2024-11-28 01:25:21 -0600

    style: use `ruff` formatting with line-length 80

--- Server/agentsServer/parkAgents/model.py
+++ Server/agentsServer/parkAgents/model.py
@@ -28,7 +28,7 @@ class ParkModel(Model):
         self.ratio_alive = 0
         self.ratio_moving = 0
         self.ratio_stopped = 0
-        
+
         # Load the map file. The map file is a text file where each character represents an agent.
         with open("park_files/2024_base.txt") as baseFile:
             lines = baseFile.readlines()
@@ -56,7 +56,7 @@ class ParkModel(Model):
                                     agent = Road(
                                         f"r_{r*self.width+c}",
                                         self,
-                                        dataDictionary[road]
+                                        dataDictionary[road],
                                     )
                                     spawned_road = True
                                     break
@@ -65,7 +65,7 @@ class ParkModel(Model):
                                 agent = Road(
                                     f"r_{r*self.width+c}",
                                     self,
-                                    dataDictionary["v"]
+                                    dataDictionary["v"],
                                 )
 
                         elif col == "s":
@@ -74,7 +74,7 @@ class ParkModel(Model):
                                     agent = Road(
                                         f"r_{r*self.width+c}",
                                         self,
-                                        dataDictionary[road]
+                                        dataDictionary[road],
                                     )
                                     spawned_road = True
                                     break
@@ -83,13 +83,10 @@ class ParkModel(Model):
                                 agent = Road(
                                     f"r_{r*self.width+c}",
                                     self,
-                                    dataDictionary["v"]
+                                    dataDictionary["v"],
                                 )
 
-                        self.grid.place_agent(
-                            agent,
-                            (c, self.height - r - 1)
-                        )
+                        self.grid.place_agent(agent, (c, self.height - r - 1))
 
                         agent = Traffic_Light(
                             f"tl_{r*self.width+c}",
@@ -135,25 +132,29 @@ class ParkModel(Model):
         if len(self.agents_by_type[Bike]) == len(self.agents_by_type[Road]):
             self.running = False
             return
-        
-        self.bikes_in_model = len(self.get_agents_of_type(Bike))
-        self.ratio_alive = self.bikes_in_model/self.bikes_spawned
-        self.ratio_arrived = self.bikes_arrived/self.bikes_spawned
 
-        self.bikes_moving = len([bike for bike in self.get_agents_of_type(Bike) if bike.moving])
+        self.bikes_in_model = len(self.get_agents_of_type(Bike))
+        self.ratio_alive = self.bikes_in_model / self.bikes_spawned
+        self.ratio_arrived = self.bikes_arrived / self.bikes_spawned
+
+        self.bikes_moving = len(
+            [bike for bike in self.get_agents_of_type(Bike) if bike.moving]
+        )
         self.bikes_stopped = self.bikes_in_model - self.bikes_moving
 
-        self.ratio_moving = self.bikes_moving/self.bikes_in_model
-        self.ratio_stopped = self.bikes_stopped/self.bikes_in_model
+        self.ratio_moving = self.bikes_moving / self.bikes_in_model
+        self.ratio_stopped = self.bikes_stopped / self.bikes_in_model
 
         self.schedule.step()
 
-        print(f"\033[38;5;197mSTEPS:\033[0m {self.schedule.steps}\n"
-              f"\033[38;5;27mBIKES SPAWNED:\033[0m {self.bikes_spawned}\n"
-              f"\033[38;5;33mIN MAP:\033[0m {self.bikes_in_model}\n"
-              f"\033[38;5;40mARRIVED:\033[0m {self.bikes_arrived} ({round(self.ratio_arrived, 2)})\n"
-              f"\033[38;5;214mMOVING:\033[0m {self.bikes_moving}\n"
-              f"\033[38;5;202mSTOPPED:\033[0m {self.bikes_stopped}")
+        print(
+            f"\033[38;5;197mSTEPS:\033[0m {self.schedule.steps}\n"
+            f"\033[38;5;27mBIKES SPAWNED:\033[0m {self.bikes_spawned}\n"
+            f"\033[38;5;33mIN MAP:\033[0m {self.bikes_in_model}\n"
+            f"\033[38;5;40mARRIVED:\033[0m {self.bikes_arrived} ({round(self.ratio_arrived, 2)})\n"
+            f"\033[38;5;214mMOVING:\033[0m {self.bikes_moving}\n"
+            f"\033[38;5;202mSTOPPED:\033[0m {self.bikes_stopped}"
+        )
 
     def spawn_bikes(self):
         """Spawn new bikes at the empty corners of the grid."""
@@ -161,7 +162,7 @@ class ParkModel(Model):
             (0, 0),
             (0, self.height - 1),
             (self.width - 1, 0),
-            (self.width - 1, self.height - 1)
+            (self.width - 1, self.height - 1),
         ]
 
         for corner in corners:
@@ -174,9 +175,14 @@ class ParkModel(Model):
             if not clear:
                 continue
 
-            destination_pos = [destination.pos for destination in self.get_agents_of_type(Destination)]
+            destination_pos = [
+                destination.pos
+                for destination in self.get_agents_of_type(Destination)
+            ]
 
-            new_bike = Bike(self.next_id(), self, self.random.choice(destination_pos))
+            new_bike = Bike(
+                self.next_id(), self, self.random.choice(destination_pos)
+            )
             self.grid.place_agent(new_bike, corner)
             self.schedule.add(new_bike)
             self.bikes_spawned += 1
@@ -184,7 +190,9 @@ class ParkModel(Model):
     def get_possible_roads(self, road):
         neighbor_roads = [
             neighbor
-            for neighbor in self.grid.get_neighbors(road.pos, moore=True, include_center=False)
+            for neighbor in self.grid.get_neighbors(
+                road.pos, moore=True, include_center=False
+            )
             if isinstance(neighbor, Road)
         ]
 
@@ -194,8 +202,8 @@ class ParkModel(Model):
             if (
                 road.direction == "Up"
                 and neighbor_road.pos[1] == road.pos[1] + 1
-                and not
-                (neighbor_road.direction == "Left"
+                and not (
+                    neighbor_road.direction == "Left"
                     and neighbor_road.pos[0] > road.pos[0]
                     or neighbor_road.direction == "Right"
                     and neighbor_road.pos[0] < road.pos[0]
@@ -205,19 +213,19 @@ class ParkModel(Model):
             elif (
                 road.direction == "Down"
                 and neighbor_road.pos[1] == road.pos[1] - 1
-                and not
-                (neighbor_road.direction == "Left"
+                and not (
+                    neighbor_road.direction == "Left"
                     and neighbor_road.pos[0] > road.pos[0]
                     or neighbor_road.direction == "Right"
                     and neighbor_road.pos[0] < road.pos[0]
-                ) 
+                )
             ):
                 possible_roads.append(neighbor_road)
             elif (
                 road.direction == "Left"
                 and neighbor_road.pos[0] == road.pos[0] - 1
-                and not
-                (neighbor_road.direction == "Up"
+                and not (
+                    neighbor_road.direction == "Up"
                     and neighbor_road.pos[1] < road.pos[1]
                     or neighbor_road.direction == "Down"
                     and neighbor_road.pos[1] > road.pos[1]
@@ -227,8 +235,8 @@ class ParkModel(Model):
             elif (
                 road.direction == "Right"
                 and neighbor_road.pos[0] == road.pos[0] + 1
-                and not
-                (neighbor_road.direction == "Up"
+                and not (
+                    neighbor_road.direction == "Up"
                     and neighbor_road.pos[1] < road.pos[1]
                     or neighbor_road.direction == "Down"
                     and neighbor_road.pos[1] > road.pos[1]
